86a5f90c24545ab7d9a819a002e25f569a140fc0
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2021-01-16 03:55:29 -0500

    A1: Updating Script.js to show all the animals

--- Activities/wheres-sausage-dog/js/script.js
+++ Activities/wheres-sausage-dog/js/script.js
@@ -2,30 +2,53 @@
 
 /*****************
 
-Title of Project
+Where's Sausage Dog?
 Author Name
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+A "Where's Wally" style game where you look for sausage dog!
 
 ******************/
+
+const NUM_ANIMAL_IMAGES = 10; //number of animal images to use
+const NUM_ANIMALS = 100; //number of animal objects to create
+
+let animalImages = []; //array to store animal images
+let animals = []; //array to store animal objects
 
 // preload()
 // Description of preload
 function preload() {
-
+  //loop initializing animal images
+  for(let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`); //pull next image from assets folder
+    animalImages.push(animalImage); //append image to end of our images array
+  }
 }
 
 
 // setup()
 // Description of setup
 function setup() {
+  createCanvas(windowWidth, windowHeight); //initialize canvas
 
+  // Create the animals
+  for(let i = 0; i < NUM_ANIMALS; i++) {
+    let x = random(0, width); //generate a random x position
+    let y = random(0, height); //generate a random y position
+    let animalImage = random(animalImages); //pull a random animal image
+    let animal = new Animal(x, y, animalImage); //construct the animal
+    animals.push(animal); //append the animal to our array of animals
+  }
 }
 
 
 // draw()
 // Description of draw()
 function draw() {
+  background(255,255,0); //set background color
 
+  //making the animals visible
+  for(let i = 0; i < animals.length; i++) {
+    animals[i].update(); //call update() and by extension display() for each animal
+  }
 }
