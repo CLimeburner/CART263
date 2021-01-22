c8edada0f5ca8510d1034817e3d91ff45581a403
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2021-01-21 22:01:25 -0500

    A2: Initializing the Slamina activity

---
+++ Activities/Slamina/js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*****************
+
+Slamina
+Tutorial: Pippin Barr
+Coded: Chip Limeburner
+
+******************/
+
+// preload()
+// Description of preload
+function preload() {
+
+}
+
+
+// setup()
+// Description of setup
+function setup() {
+
+}
+
+
+// draw()
+// Description of draw()
+function draw() {
+
+}
