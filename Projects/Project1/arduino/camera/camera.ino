9e94b6757e733a4d25a99774ee06c6e8685819d3
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2021-01-23 23:57:45 -0500

    P1: Added directional button functionality, formatting to byte for data to be passed to server, and cleaned up and commented code

--- Projects/Project1/arduino/camera/camera.ino
+++ Projects/Project1/arduino/camera/camera.ino
@@ -7,25 +7,58 @@
   camera is held up in front of the face. 
 */
 
+//assign pins for the buttons
+const int upPin = 2;
+const int downPin = 3;
+const int leftPin = 4;
+const int rightPin = 5;
 
-int sensorValue;
+int up = 0; //binary indicator of up button being pressed
+int down = 0; //binary indicator of down button being pressed
+int left = 0; //binary indicator of left button being pressed
+int right = 0; //binary indicator of right button being pressed
 
-int sensorLow = 1023;
+int sensorValue; //the raw analog value from the photoresistor
 
-int sensorHigh = 0;
+int sensorHigh = 0; //binary representation of if the photoresistor is "high" or "low" based on an experimentally determined cutoff
+
+int serialOutputByte = 0; //variable to store the byte to send to the server
 
 
+// setup()
+// function that initializes and starts the material we'll need in the main loop.
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //start the serial output
 
-
+  //initialize button pins
+  pinMode(upPin, INPUT);
+  pinMode(downPin, INPUT);
+  pinMode(leftPin, INPUT);
+  pinMode(rightPin, INPUT);
 }
 
 
+// loop()
+// function that will loop endlessless. Pulls our input data and formats it to be passed to the server.
 void loop() {
-  sensorValue = analogRead(A0);
+  //read the button pin values
+  up = !digitalRead(upPin);
+  down = !digitalRead(downPin);
+  left = !digitalRead(leftPin);
+  right = !digitalRead(rightPin);
 
-  Serial.println(sensorValue);
+  sensorValue = analogRead(A0); //read the analog value from the photoresistor
 
-  delay(10);
+  //translate the analog photoresistor value to a digital one
+  if (sensorValue > 600) {
+    sensorHigh = 1;
+  } else {
+    sensorHigh = 0;
+  }
+
+  serialOutputByte = (1*up) + (2*down) + (4*left) + (8*right) + (16*sensorHigh); //convert all our binary values to a passable byte
+
+  Serial.println(serialOutputByte); //output byte
+  
+  delay(10); //wait a bit
 }
