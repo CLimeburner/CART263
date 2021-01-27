1c55bf83c6293a218f44560a3c6e685a701dbe14
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2021-01-27 03:13:29 -0500

    A3: Implemented a name prompt and display spy profile information as redacted on the screen

--- Activities/Spy Profile Generator/js/script.js
+++ Activities/Spy Profile Generator/js/script.js
@@ -10,6 +10,14 @@ Description
 
 ******************/
 
+
+let spyProfile = {
+  name: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  secretWeapon: `**REDACTED**`,
+  password: `**REDACTED**`
+};
+
 // preload()
 // Description of preload
 function preload() {
@@ -20,12 +28,30 @@ function preload() {
 // setup()
 // Description of setup
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  spyProfile.name = prompt(`Agent! What is your name?`);
 }
 
 
 // draw()
 // Description of draw()
 function draw() {
+  background(255);
 
+  let profile = `** FOR YOUR EYES ONLY **
+
+  Name: ${spyProfile.name}
+  Alias: ${spyProfile.alias}
+  Secret Weapon: ${spyProfile.secretWeapon}
+  Password: ${spyProfile.password}`
+  ;
+
+  push();
+  textFont(`Courrier, monospace`);
+  textSize(24);
+  textAlign(TOP, LEFT);
+  fill(0);
+  text(profile, 100, 100);
+  pop();
 }
