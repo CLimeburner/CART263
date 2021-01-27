2905063064d6368118a7193ac4d30e4c3c897cd7
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2021-01-26 23:08:19 -0500

    A3: Initialize code for spy profile generator activity

---
+++ Activities/Spy Profile Generator/js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/*****************
+
+Spy Porfile Generator
+Tutorial: Pippin Barr
+Coded: Chip Limeburner
+
+Description
+
+******************/
+
+// preload()
+// Description of preload
+function preload() {
+
+}
+
+
+// setup()
+// Description of setup
+function setup() {
+
+}
+
+
+// draw()
+// Description of draw()
+function draw() {
+
+}
