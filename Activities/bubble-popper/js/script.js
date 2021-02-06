845c7230edb10c2c9ce442d39a8573117120ed5c
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2021-02-06 12:43:02 -0500

    A4: Implemented drawing a pin based on index finger position

--- Activities/bubble-popper/js/script.js
+++ Activities/bubble-popper/js/script.js
@@ -45,5 +45,32 @@ function setup() {
 // draw()
 // Description of draw()
 function draw() {
+  background(0); //make the background black
 
+  if (predictions.length > 0) {
+    //break down data from predictions
+    let hand = predictions[0];
+    let index = hand.annotations.indexFinger;
+    let tip = index[3];
+    let base = index[0];
+    let tipX = tip[0];
+    let tipY = tip[1];
+    let baseX = base[0];
+    let baseY = base[1];
+
+    //draw the pin shaft
+    push();
+    noFill();
+    stroke(255);
+    strokeWeight(2);
+    line(baseX, baseY, tipX, tipY);
+    pop();
+
+    //draw the pin ball
+    push();
+    noStroke();
+    fill(255, 0, 0);
+    ellipse(baseX, baseY, 20);
+    pop();
+  }
 }
