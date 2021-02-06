92c5286ebeae483da70bc200979f6bfdb538d0fa
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2021-02-06 12:27:58 -0500

    A4: Implemented basic handpose prediction functionality

--- Activities/bubble-popper/js/script.js
+++ Activities/bubble-popper/js/script.js
@@ -10,17 +10,35 @@ An activity using machine learning!
 
 ******************/
 
-// preload()
-// Description of preload
-function preload() {
 
-}
+let video = undefined; //a variable to store the user's webcam
+
+let handpose = undefined; //a vairable to store the hand pose model
+
+let predictions = []; //a variable to store predictions from the hand pose model
 
 
 // setup()
 // Description of setup
 function setup() {
+  createCanvas(640, 480); //create the canvas
 
+  //access the user's webcam and hide it
+  video = createCapture(VIDEO);
+  video.hide();
+
+  //load the handpose model
+  handpose = ml5.handpose(video, {
+    flipHorizontal: true
+  }, function() {
+    console.log(`Model loaded.`);
+  });
+
+  //listen for predictions
+  handpose.on(`predict`, function(results) {
+    console.log(results);
+    predictions = results;
+  });
 }
 
 
