4a30cba7ccf2476c18eabc543ee5cb8d0360435f
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2021-03-06 15:38:03 -0500

    E6: Starting code for exercise 6

---
+++ Exercises/raving-redactionist-plus-plus/js/script.js
@@ -0,0 +1,29 @@
+/**
+Raving Redactionist++
+
+Coded by: Chip Limeburner
+
+*/
+
+"use strict";
+
+$(`.top-secret`).on(`click`, redact);
+
+setInterval(revelation, 500);
+
+function redact(event) {
+  $(this).removeClass(`revealed`);
+  $(this).addClass(`redacted`);
+}
+
+function revelation() {
+  $(`.redacted`).each(attemptReveal);
+}
+
+function attemptReveal() {
+  let r = Math.random();
+  if(r < 0.1) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
+  }
+}
