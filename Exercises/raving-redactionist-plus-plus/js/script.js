d8b35ca68568163c326a72040d76514d4f9d6796
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2021-03-06 15:51:25 -0500

    E6: Added a ramp in difficulty

--- Exercises/raving-redactionist-plus-plus/js/script.js
+++ Exercises/raving-redactionist-plus-plus/js/script.js
@@ -7,22 +7,31 @@ Coded by: Chip Limeburner
 
 "use strict";
 
-$(`.top-secret`).on(`click`, redact);
+let difficulty = 0.05; //a variable to allow us to manipulate the probablity of elements becoming visible
 
-setInterval(revelation, 500);
+$(`.top-secret`).on(`click`, redact); //add an event listener for clicking on top-secret elements
 
+setInterval(revelation, 750); //every "timer" interval, call the revelation function
+
+// redact(event)
+// hide elements that have been clicked
 function redact(event) {
   $(this).removeClass(`revealed`);
   $(this).addClass(`redacted`);
 }
 
+//  revelation()
+//  for each redacted element, attempt to reveal it
 function revelation() {
   $(`.redacted`).each(attemptReveal);
+  difficulty += 0.005; //increase the probability of a reveal
 }
 
+// attemptReveal()
+// reveals elements at a random probabilityx
 function attemptReveal() {
   let r = Math.random();
-  if(r < 0.1) {
+  if(r < difficulty) {
     $(this).removeClass(`redacted`);
     $(this).addClass(`revealed`);
   }
