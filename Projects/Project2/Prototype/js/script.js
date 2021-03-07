9e6267a5ba7f0db063ee99cfb66f81b8306f75c1
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2021-03-07 07:43:44 -0500

    P2: Implemented basic framework for constructing a live-editable iframe

--- Projects/Project2/Prototype/js/script.js
+++ Projects/Project2/Prototype/js/script.js
@@ -1,1 +1,61 @@
 "use strict";
+
+
+let newHTMLDocument;
+let demoURL;
+
+
+
+
+$(`#interface-container`).click(compileHtmlDoc);
+
+
+
+
+
+
+
+
+function compileHtmlDoc() {
+
+newHTMLDocument = ``;
+
+newHTMLDocument +=
+`<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0>
+
+    <title>Demo-frame</title>
+
+    <style>`
+
+newHTMLDocument +=
+`body {height: 100%;, width: 100%; background-color: blue;}
+`
+
+
+
+
+newHTMLDocument +=
+    `</style>
+  </head>
+  <body>`
+
+
+
+
+
+
+
+newHTMLDocument +=
+`</body>
+</html>`
+
+//console.log(newHTMLDocument);
+let htmlBlob = new Blob([newHTMLDocument], {type : 'text/html'});
+demoURL = URL.createObjectURL(htmlBlob);
+console.log(demoURL)
+$(`iframe`).attr(`src`, demoURL);
+}
