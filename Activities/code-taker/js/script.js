263937ff9fa6266aedc02a619ca73c429ddfdec6
CLimeburner <74136575+CLimeburner@users.noreply.github.com>
2021-03-13 15:01:21 -0500

    A7: Implemented conclusion popup

--- Activities/code-taker/js/script.js
+++ Activities/code-taker/js/script.js
@@ -8,6 +8,15 @@ Coded by: Chip Limeburner
 
 "use strict";
 
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
+
 $(`.secret`).one(`mouseover`, function(event) {
   $(this).addClass(`found`, 500);
   $(this).draggable({
@@ -21,5 +30,9 @@ $(`#answer`).droppable({
     $(this).append(letter);
     ui.draggable.draggable(`disable`);
     ui.draggable.removeClass(`found`);
+
+    if($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
   }
 });
